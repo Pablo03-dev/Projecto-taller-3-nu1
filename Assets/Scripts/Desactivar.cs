9f9281cc7283b4984727db0bc977fed25628f2c6
Pablo03-dev <pablo.villegasa@alumnos.arcos.cl>
2022-05-04 09:01:42 -0400

    mejoras

---
+++ Assets/Scripts/Desactivar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desactivar : MonoBehaviour
+{
+public void DesactivarObjeto()
+    {
+        gameObject.SetActive(false);
+    }
+}
