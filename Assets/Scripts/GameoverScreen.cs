beeab5421d326983856f0d979573b1e5c04fb252
Pablo03-dev <84738819+Pablo03-dev@users.noreply.github.com>
2022-05-09 21:03:51 -0400

    algo muy raro

--- Assets/Scripts/GameoverScreen.cs
+++ Assets/Scripts/GameoverScreen.cs
@@ -7,35 +7,47 @@ using UnityEngine.SceneManagement;
 public class GameoverScreen : MonoBehaviour
 {
     public Text pointsText;
-    private bool juegoPausado = false;
+    private bool JuegoPausado = false;
 
     public void Setup(int score)
     {
         gameObject.SetActive(true);
         pointsText.text = score.ToString() + " Puntos ";
+
+        JuegoPausado = true;
+        Pausa();
+    }
+
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+        JuegoPausado = true;
     }
 
     public void RestartButton()
     {
         SceneManager.LoadScene("Base juego");
+        Time.timeScale = 1f;
+        JuegoPausado = false;
     }
 
     public void ExittButton()
     {
         SceneManager.LoadScene("Menu Principal");
+        Time.timeScale = 1f;
     }
 
-    private void Update()
-    {
-        if (juegoPausado)
-        {
-            Pausa();
-        }
-    }
-    public void Pausa()
-    {
-        juegoPausado = true;
-        Time.timeScale = 0f;
+    //private void Update()
+    //{
+    //    if (juegoPausado)
+    //    {
+    //        Pausa();
+    //    }
+    //}
+    //public void Pausa()
+    //{
+    //    Time.timeScale = 0f;
+    //    juegoPausado = true;
         
-    }
+    //}
 }
